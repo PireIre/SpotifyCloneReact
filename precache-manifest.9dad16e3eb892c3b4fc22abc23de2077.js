71ad22ee40a9ca7897357219da5d04b500a3a063
PireIre <bozoire@gmail.com>
2020-04-26 13:01:43 +0200

    Updates

---
+++ precache-manifest.9dad16e3eb892c3b4fc22abc23de2077.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1cfb25c0633d813c53c9640da7e48b1",
+    "url": "/SpotifyCloneReact/index.html"
+  },
+  {
+    "revision": "1c09601bc1e41ff49b3a",
+    "url": "/SpotifyCloneReact/static/css/main.cfdd811d.chunk.css"
+  },
+  {
+    "revision": "ce003783e65d96000c21",
+    "url": "/SpotifyCloneReact/static/js/2.eb29562f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SpotifyCloneReact/static/js/2.eb29562f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c09601bc1e41ff49b3a",
+    "url": "/SpotifyCloneReact/static/js/main.0e73feb1.chunk.js"
+  },
+  {
+    "revision": "4b7aa7f8da2667938c76",
+    "url": "/SpotifyCloneReact/static/js/runtime-main.8d97c9d7.js"
+  },
+  {
+    "revision": "d19bc82bbf9fc2d9009e4708595a4a66",
+    "url": "/SpotifyCloneReact/static/media/Album-Header.d19bc82b.png"
+  },
+  {
+    "revision": "8549ffd477cd7dbc016d36a90b784c62",
+    "url": "/SpotifyCloneReact/static/media/All-The-Small-Things.8549ffd4.mp3"
+  },
+  {
+    "revision": "b991053463379ba84ac602bc514455e4",
+    "url": "/SpotifyCloneReact/static/media/Beautiful-Day.b9910534.mp3"
+  },
+  {
+    "revision": "22dba4fb2196f8f1f37039b3464e157b",
+    "url": "/SpotifyCloneReact/static/media/Chasing-Cars.22dba4fb.mp3"
+  },
+  {
+    "revision": "6c523428bc7f17e1086b0e662ee63a89",
+    "url": "/SpotifyCloneReact/static/media/Clocks.6c523428.mp3"
+  },
+  {
+    "revision": "d9e45c4f3cb174cf9862270e7507a454",
+    "url": "/SpotifyCloneReact/static/media/Crazy.d9e45c4f.mp3"
+  },
+  {
+    "revision": "59a0cbfdd1e8ada108b0ca46140ea4b6",
+    "url": "/SpotifyCloneReact/static/media/Dragostea-Din-Tei.59a0cbfd.mp3"
+  },
+  {
+    "revision": "ea85d5a5077901dbf7ccfaf6eda71b6a",
+    "url": "/SpotifyCloneReact/static/media/Human.ea85d5a5.mp3"
+  },
+  {
+    "revision": "abc3598cc7d805a8e839aaf47fe58258",
+    "url": "/SpotifyCloneReact/static/media/Lonely.abc3598c.mp3"
+  },
+  {
+    "revision": "b8f37be9fd417089ad01b237b8eaf45d",
+    "url": "/SpotifyCloneReact/static/media/Speed-Of-Sound.b8f37be9.mp3"
+  }
+]);
